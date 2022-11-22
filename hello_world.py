9d155d6874c2fca01090f179503590af6bb27ed3
FeNToNelele <sarosigab@gmail.com>
2022-11-22 12:52:58 +0100

    Edited function documentation

--- hello_world.py
+++ hello_world.py
@@ -1,7 +1,7 @@
 """Program to print Hello world!"""
 
-"""The function that print Hello world!"""
 def hello_world():
+    """The function that print Hello world!"""
     print("Hello world!")
 
 hello_world()
