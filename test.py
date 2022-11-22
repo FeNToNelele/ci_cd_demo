2daf7a9bbaddadd04fb7c49a3f6f8777d027f4bb
FeNToNelele <sarosigab@gmail.com>
2022-11-22 13:14:09 +0100

    Fixed unit test method calls

--- test.py
+++ test.py
@@ -17,17 +17,17 @@ class testEquationSolverClass(unittest.TestCase):
 
     def test_secondDegreeEquation_case_for_solutions(self):
         print("running test 1")
-        self.assertEqual(self.tst.secondDegreeEquationSolver(1, 2, 8), (None, None), msg="D<0 hiba")
+        self.assertEqual(self.tst.second_degree_equation_solver(1, 2, 8), (None, None), msg="D<0 hiba")
         print("running test 2")
-        self.assertEqual(self.tst.secondDegreeEquationSolver(1, -14, 49), (7, 7), msg="D=1 hiba")
+        self.assertEqual(self.tst.second_degree_equation_solver(1, -14, 49), (7, 7), msg="D=1 hiba")
         print("running test 3")
-        self.assertEqual(self.tst.secondDegreeEquationSolver(1, 6, 8), (-2, -4), msg="D=2 hiba")
+        self.assertEqual(self.tst.second_degree_equation_solver(1, 6, 8), (-2, -4), msg="D=2 hiba")
 
     #TypeError visszadobásának tesztelése
     def test_secondDegreeEquation_case_TypeError(self):
         print("running TypeError test")
         with self.assertRaises(TypeError):
-            result = self.tst.secondDegreeEquationSolver(True, 2, 4)
+            result = self.tst.second_degree_equation_solver(True, 2, 4)
 
 if __name__ == '__main__':
     unittest.main()
